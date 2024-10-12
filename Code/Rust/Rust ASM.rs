928b741aece08e0a7c674315fa7fa48ad79b6ae4
V-Shiro <104509168+V-Shiro@users.noreply.github.com>
2024-10-12 19:42:21 +0200

    works

--- Code/Rust/Rust ASM.rs
+++ Code/Rust/Rust ASM.rs
@@ -1,74 +1,27 @@
+// https://doc.rust-lang.org/std/net/index.html
 // https://bluejekyll.github.io/blog/posts/multicasting-in-rust/
+// https://doc.rust-lang.org/std/net/struct.UdpSocket.html
 
-// vllt: https://github.com/bluejekyll/multicast-example/blob/master/src/lib.rs
+use std::net::{SocketAddr, UdpSocket};
+use std::str;
 
-// windows bind problem 
+fn main() -> std::io::Result<()> {
+    // Multicast address 
+    let multicastAddr = Ipv4Addr::new(232, 0, 0, 0);
+    
+    // Create a UDP socket
+    let socket = UdpSocket::bind("0.0.0.0:1900")?;
 
-use std::io;
-use std::time::Duration;
+    // add multicast membership
+    let netInt = Ipv4Addr::new(0, 0, 0, 0);
+    socket.join_multicast_v4(multicastAddr, netInt)?;
 
-use socket2::{Domain, Protocol, SockAddr, Socket, Type};
-
-// this will be common for all our sockets
-fn new_socket(addr: &SocketAddr) -> io::Result<Socket> {
-    let domain = if addr.is_ipv4() {
-        Domain::ipv4()
-    } else {
-        Domain::ipv6()
-    };
-
-    let socket = Socket::new(domain, Type::dgram(), Some(Protocol::udp()))?;
-
-    // we're going to use read timeouts so that we don't hang waiting for packets
-    socket.set_read_timeout(Some(Duration::from_millis(100)))?;
-
-    Ok(socket)
-}
-
-fn join_multicast(addr: SocketAddr) -> io::Result<Socket> {
-    let ip_addr = addr.ip();
-
-    let socket = new_socket(&addr)?;
-
-    // depending on the IP protocol we have slightly different work
-    match ip_addr {
-        IpAddr::V4(ref mdns_v4) => {
-            // join to the multicast address, with all interfaces
-            socket.join_multicast_v4(mdns_v4, &Ipv4Addr::new(0, 0, 0, 0))?;
-        }
-        IpAddr::V6(ref mdns_v6) => {
-            // join to the multicast address, with all interfaces (ipv6 uses indexes not addresses)
-            socket.join_multicast_v6(mdns_v6, 0)?;
-            socket.set_only_v6(true)?;
-        }
-    };
-
-    // bind us to the socket address.
-    socket.bind(&SockAddr::from(addr))?;
-    Ok(socket)
-}
-
-
-
-
-
-
-/// On Windows, unlike all Unix variants, it is improper to bind to the multicast address
-///
-/// see https://msdn.microsoft.com/en-us/library/windows/desktop/ms737550(v=vs.85).aspx
-#[cfg(windows)]
-fn bind_multicast(socket: &Socket, addr: &SocketAddr) -> io::Result<()> {
-    let addr = match *addr {
-        SocketAddr::V4(addr) => SocketAddr::new(Ipv4Addr::new(0, 0, 0, 0).into(), addr.port()),
-        SocketAddr::V6(addr) => {
-            SocketAddr::new(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0).into(), addr.port())
-        }
-    };
-    socket.bind(&socket2::SockAddr::from(addr))
-}
-
-/// On unixes we bind to the multicast address, which causes multicast packets to be filtered
-#[cfg(unix)]
-fn bind_multicast(socket: &Socket, addr: &SocketAddr) -> io::Result<()> {
-    socket.bind(&socket2::SockAddr::from(*addr))
+    let mut buf = [0; 1024];
+    loop {
+        let (len, src) = socket.recv_from(&mut buf)?;
+        let msg = str::from_utf8(&buf[..len]).expect("Failed to convert message to UTF-8");
+        println!("Received {} bytes from {}: {}", len, src, msg);
+    }
+    // drop membership
+    socket.leave_multicast_v4(multicastAddr, netInt)?;
 }
